16a946e3dc6abb7e48a4c4a54b5668ebabed1daa
basalgangliagct <fantasypad@163.com>
2023-04-19 02:43:54 +0800

    add tableformat

---
+++ Work/tableformat.py
@@ -0,0 +1,45 @@
+class TableFormatter:
+    def headings(self,headers):
+        '''
+        Emit the table headings.
+        '''
+        for h in headers:
+            print(f'{h:>10s}',end=' ')
+        print()
+        print(('-'*10+' ')*len(headers))
+    # raise NotImplementedError()
+
+    def row(self,rowdata):
+        '''
+        Emit a single row of table data.
+        '''
+        for d in rowdata:
+            print(f'{d:>10s}',end=' ')
+        print()
+    # raise NotImplementedError()
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format.
+    '''
+    def headings(self,headers):
+        print(','.join(headers))
+
+    def row(self,rowdata):
+        print(','.join(rowdata))
+
+class HTMLTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in HTML format.
+    '''
+    def headings(self,headers):
+        print('<tr>',end='')
+        for h in headers:
+            print(f'<th>{h}</th>',end='')
+        print('</tr>')
+    
+    def row(self,rowdata):
+        print('<tr>',end='')
+        for r in rowdata:
+            print(f'<td>{r}</td>',end='')
+        print('</tr>')
